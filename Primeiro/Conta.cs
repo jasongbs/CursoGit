7d47aa920ee7c15e2c89f73293953baf81629b93
Jason <jasongbds@gmail.com>
2020-01-20 16:26:46 -0300

    Programa Iniado, realizando algumas operacoes

---
+++ Primeiro/Conta.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Primeiro
+{
+    class Conta
+    {
+        public string Titular { get; set; }
+        public int NConta { get; set; }
+        public double Saldo { get; set; }
+      
+      public void deposito(double valor){
+            Saldo += valor;
+        }
+
+        public void saque(double valor)
+        {
+            Saldo -= valor + 5;
+        }
+
+        public void listarDados()
+        {
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine("Conta " + NConta + ", Titular: " + Titular + ", Saldo: " + Saldo);
+        }
+
+    }
+}
---
+++ Primeiro/Program.cs
@@ -0,0 +1,37 @@
+﻿using Primeiro;
+using System;
+
+namespace PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta c = new Conta();
+     
+            Console.Write("Entre com número da conta: ");
+            c.NConta = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            c.Titular = Console.ReadLine();
+
+            Console.Write("Havera depósito inicial (s/n)?");
+            char operacao = char.Parse(Console.ReadLine());
+
+            if (operacao == 's' || operacao == 'S'){
+                Console.WriteLine("Entre um valor para depósito incial: ");
+                c.deposito(double.Parse(Console.ReadLine()));
+            }
+
+            c.listarDados();
+
+            Console.WriteLine("Entre um valor para depósito: ");
+            c.deposito(double.Parse(Console.ReadLine()));
+            c.listarDados();
+
+            Console.WriteLine("Entre um valor para Saque: ");
+            c.saque(double.Parse(Console.ReadLine()));
+            c.listarDados();
+        }
+    }
+}
+  
